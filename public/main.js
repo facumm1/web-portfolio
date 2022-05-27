773820f9ef45b58857651880196dc28fdbc01152
Facu <facundomamani120@gmail.com>
2022-05-27 00:16:39 -0300

    Seccion about me realizada, desarrollando seccion portfolio

--- public/main.js
+++ public/main.js
@@ -2,5 +2,5 @@ const headerToggler = document.querySelector('.toggler');
 const headerNav = document.querySelector('.nav');
 const headerCloser = document.querySelector('.closer');
 
-headerToggler.addEventListener('click', () => headerNav.style.left = 0);
-headerCloser.addEventListener('click', () => headerNav.style.left = 150 + '%');
+headerToggler.addEventListener('click', () => headerNav.style.top = 0);
+headerCloser.addEventListener('click', () => headerNav.style.top = -150 + '%');
